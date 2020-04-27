8712f76be798e4eccf8554ad87f590ad5cb9efab
amalrachdi <64441318+amalrachdi@users.noreply.github.com>
2020-04-27 22:17:58 +0000

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,118 @@
+<div id="footer"><!-- #footer Begin -->
+    <div class="container"><!-- container Begin -->
+        <div class="row"><!-- row Begin -->
+            <div class="col-sm-6 col-md-3"><!-- col-sm-6 col-md-3 Begin -->
+               
+               <h4>Pages</h4>
+                
+                <ul><!-- ul Begin -->
+                    <li><a href="cart.php">Panier</a></li>
+                    <li><a href="contact.php">Nous contacter</a></li>
+                    <li><a href="shop.php">Boutique</a></li>
+                    <li><a href="checkout.php">My Account</a></li>
+                </ul><!-- ul Finish -->
+                
+                <hr>
+                
+                <h4>Section utilisateur</h4>
+                
+                <ul><!-- ul Begin -->
+                    <li><a href="checkout.php">Login</a></li>
+                    <li><a href="customer_register.php">Register</a></li>
+                </ul><!-- ul Finish -->
+                
+                <hr class="hidden-md hidden-lg hidden-sm">
+                
+            </div><!-- col-sm-6 col-md-3 Finish -->
+            
+            <div class="com-sm-6 col-md-3"><!-- col-sm-6 col-md-3 Begin -->
+                
+                <h4>Top catégories de produits</h4>
+                
+                <ul><!-- ul Begin -->
+                    <li><a href="#">Vestes</a></li>
+                    <li><a href="#">Accessoires</a></li>
+                    <li><a href="#">Manteaux</a></li>
+                    <li><a href="#">Des chaussures</a></li>
+                    <li><a href="#">T-Shirts</a></li>
+                </ul><!-- ul Finish -->
+                
+                <hr class="hidden-md hidden-lg">
+                
+            </div><!-- col-sm-6 col-md-3 Finish -->
+            
+            <div class="col-sm-6 col-md-3"><!-- col-sm-6 col-md-3 Begin -->
+                
+                <h4>Trouve nous</h4>
+                
+                <p><!-- p Start -->
+                    
+                    <strong>SHOPPING</strong>
+                    <br/>AGADIR
+                    <br/>AGADIR
+                    <br/>0000-0683-1111
+                    <br/>SHOPPING@gmail.com
+                    <br/><strong>SHOPPING</strong>
+                    
+                </p><!-- p Finish -->
+                
+                <a href="contact.php">Consultez notre page de contact</a>
+                
+                <hr class="hidden-md hidden-lg">
+                
+            </div><!-- col-sm-6 col-md-3 Finish -->
+            
+            <div class="col-sm-6 col-md-3">
+                
+                <h4>Obtenez les nouvelles</h4>
+                
+                <p class="text-muted">
+                   Ne manquez pas nos derniers produits de mise à jour.
+                </p>
+                
+                <form action="" method="post"><!-- form begin -->
+                    <div class="input-group"><!-- input-group begin -->
+                        
+                        <input type="text" class="form-control" name="email">
+                        
+                        <span class="input-group-btn"><!-- input-group-btn begin -->
+                            
+                            <input type="submit" value="souscrire" class="btn btn-default">
+                            
+                        </span><!-- input-group-btn Finish -->
+                        
+                    </div><!-- input-group Finish -->
+                </form><!-- form Finish -->
+                
+                <hr>
+                
+                <h4>Rester en contact</h4>
+                
+                <p class="social">
+                    <a href="#" class="fa fa-facebook"></a>
+                    <a href="#" class="fa fa-twitter"></a>
+                    <a href="#" class="fa fa-instagram"></a>
+                    <a href="#" class="fa fa-google-plus"></a>
+                    <a href="#" class="fa fa-envelope"></a>
+                </p>
+                
+            </div>
+        </div><!-- row Finish -->
+    </div><!-- container Finish -->
+</div><!-- #footer Finish -->
+
+
+<div id="copyright"><!-- #copyright Begin -->
+    <div class="container"><!-- container Begin -->
+        <div class="col-md-6"><!-- col-md-6 Begin -->
+            
+            <p class="pull-left">&copy; 2020 shopping</p>
+            
+        </div><!-- col-md-6 Finish -->
+        <div class="col-md-6"><!-- col-md-6 Begin -->
+            
+            <!-- <p class="pull-right">Theme by: <a href="#">shopping</a></p> -->
+            
+        </div><!-- col-md-6 Finish -->
+    </div><!-- container Finish -->
+</div><!-- #copyright Finish -->
---
+++ includes/sidebar.php
@@ -0,0 +1,37 @@
+<div class="panel panel-default sidebar-menu"><!-- panel panel-default sidebar-menu Begin -->
+    <div class="panel-heading"><!-- panel-heading Begin -->
+        <h3 class="panel-title">Catégories de produits</h3>
+    </div><!-- panel-heading Finish -->
+    
+    <div class="panel-body"><!-- panel-body Begin -->
+        <ul class="nav nav-pills nav-stacked category-menu"><!-- nav nav-pills nav-stacked category-menu Begin -->
+            
+            <li><a href="#">Vestes</a></li>
+            <li><a href="#">Accessoires</a></li>
+            <li><a href="#">Manteaux</a></li>
+            <li><a href="#">Des chaussures</a></li>
+            <li><a href="#">T-Shirt</a></li>
+            
+        </ul><!-- nav nav-pills nav-stacked category-menu Finish -->
+    </div><!-- panel-body Finish -->
+    
+</div><!-- panel panel-default sidebar-menu Finish -->
+
+
+<div class="panel panel-default sidebar-menu"><!-- panel panel-default sidebar-menu Begin -->
+    <div class="panel-heading"><!-- panel-heading Begin -->
+        <h3 class="panel-title">Categories</h3>
+    </div><!-- panel-heading Finish -->
+    
+    <div class="panel-body"><!-- panel-body Begin -->
+        <ul class="nav nav-pills nav-stacked category-menu"><!-- nav nav-pills nav-stacked category-menu Begin -->
+            
+            <li><a href="#">Homme</a></li>
+            <li><a href="#">femme</a></li>
+            <li><a href="#">Des gamins</a></li>
+            <li><a href="#">Autres</a></li>
+            
+        </ul><!-- nav nav-pills nav-stacked category-menu Finish -->
+    </div><!-- panel-body Finish -->
+    
+</div><!-- panel panel-default sidebar-menu Finish -->
